6efca6ee1e3f376be92a7a7d551fe52e66c81470
caslabs <jeraldydcascayan@gmail.com>
2018-09-17 14:18:42 -1000

    ye

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+      artist: 'Howard The Alien',
+      genre: 'Hip-Hop',
+      song: 'Money Longer Alien',
+      image: 'https://i.ytimg.com/vi/EBxNE2G640Q/maxresdefault.jpg'
+    },
+    {
+        artist: 'Smash Mouth',
+        genre: 'Indie',
+        song: 'All-Star',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/16/All_star.jpg/220px-All_star.jpg'
+      },
+      {
+        artist: 'Ween',
+        genre: 'Alternative',
+        song: 'Ocean Man',
+        image: 'http://images.genius.com/2e3c9836a7db1db0063978358e19659d.640x640x1.jpg'
+      },    {
+        artist: 'Gary Jules',
+        genre: 'Rock',
+        song: 'Mad World',
+        image: 'https://images.rapgenius.com/9c5b21ae6fa3da94704da3777f87e0be.400x400x1.jpg'
+      },    {
+        artist: 'SayMaxWell, MiatriSs',
+        genre: 'Electronic',
+        song: 'We Are Number One',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+      }
+  ]
