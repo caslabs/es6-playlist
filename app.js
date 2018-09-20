d7876c8790690bb1ee911c7580c3db571650d14e
caslabs <jeraldydcascayan@gmail.com>
2018-09-19 14:41:19 -1000

    oof

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log('data', data);
+
+console.log('data[0].artist', data[0].artist);
+
+document.querySelector('#test').addEventListener('click',(event) => {
+    const oof = document.createElement('p');
+    document.querySelector('#mscCont').innerHTML = ""
+    const elect2 = data.filter(music => {
+        return music.genre == 'Electronic' ;
+    }).forEach((e) => {
+        return oof.innerHTML+=`${e.song} by ${e.artist} <br>`
+    })
+
+
+    document.querySelector('#mscCont').appendChild(oof)
+});
+
+document.querySelector('#test2').addEventListener('click',(event) => {
+    const oof = document.createElement('p');
+    document.querySelector('#mscCont').innerHTML = ""
+    const elect2 = data.filter(music => {
+        return music.genre == 'Hip-Hop' ;
+    }).forEach((e) => {
+        return oof.innerHTML+=`${e.song} by ${e.artist} <br>`
+    })
+
+
+    document.querySelector('#mscCont').appendChild(oof)
+});
--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 const data = [
     {
       artist: 'Howard The Alien',
-      genre: 'Hip-Hop',
+      genre: 'Electronic',
       song: 'Money Longer Alien',
       image: 'https://i.ytimg.com/vi/EBxNE2G640Q/maxresdefault.jpg'
     },
     {
         artist: 'Smash Mouth',
-        genre: 'Indie',
+        genre: 'Electronic',
         song: 'All-Star',
         image: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/16/All_star.jpg/220px-All_star.jpg'
       },
       {
         artist: 'Ween',
-        genre: 'Alternative',
+        genre: 'Electronic',
         song: 'Ocean Man',
         image: 'http://images.genius.com/2e3c9836a7db1db0063978358e19659d.640x640x1.jpg'
       },    {
         artist: 'Gary Jules',
-        genre: 'Rock',
+        genre: 'Electronic',
         song: 'Mad World',
         image: 'https://images.rapgenius.com/9c5b21ae6fa3da94704da3777f87e0be.400x400x1.jpg'
       },    {
@@ -26,5 +26,48 @@ const data = [
         genre: 'Electronic',
         song: 'We Are Number One',
         image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+      },
+
+      {
+        artist: 'SayMaxWell, MiatriSs5',
+        genre: 'Electronic',
+        song: 'Before the bhombs fell',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+      },
+      {
+        artist: 'Tim The Donut',
+        genre: 'Hip-Hop',
+        song: 'Money Happu Man',
+        image: 'https://i.ytimg.com/vi/EBxNE2G640Q/maxresdefault.jpg'
+      },
+      {
+          artist: 'Smash Pies',
+          genre: 'Hip-Hop',
+          song: 'Happu-Starzs',
+          image: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/16/All_star.jpg/220px-All_star.jpg'
+        },
+        {
+          artist: 'TWeen3',
+          genre: 'Hip-Hop',
+          song: 'Ocean Women',
+          image: 'http://images.genius.com/2e3c9836a7db1db0063978358e19659d.640x640x1.jpg'
+        },    {
+          artist: 'Gary Jules4',
+          genre: 'Hip-Hop',
+          song: 'Happu Oof',
+          image: 'https://images.rapgenius.com/9c5b21ae6fa3da94704da3777f87e0be.400x400x1.jpg'
+        },    {
+          artist: 'SayMaxWell, MiatriSs5',
+          genre: 'Hip-Hop',
+          song: 'We Are Number Loof',
+          image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+        },
+        {
+        artist: 'SayMaxWell, MiatriSs5',
+        genre: 'Hip-Hop',
+        song: 'Life as we know it',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
       }
+
+      
   ]
