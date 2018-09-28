95d5c02e8c20d44f05721bd76005def5318f29d6
c0p <jeraldydcascayan@gmail.com>
2018-09-28 12:16:36 -1000

    mvp

--- app.js
+++ app.js
@@ -3,27 +3,79 @@ console.log('data', data);
 console.log('data[0].artist', data[0].artist);
 
 document.querySelector('#test').addEventListener('click',(event) => {
-    const oof = document.createElement('p');
-    document.querySelector('#mscCont').innerHTML = ""
-    const elect2 = data.filter(music => {
-        return music.genre == 'Electronic' ;
-    }).forEach((e) => {
-        return oof.innerHTML+=`${e.song} by ${e.artist} <br>`
+    var filterRock = data.filter(function (song){
+        return song.genre === "Electronic";
     })
-
-
-    document.querySelector('#mscCont').appendChild(oof)
+    .map(function (genre) {
+       return  `
+       <div id="songCont">
+       <img class="songFeat image" src=${genre.image}>
+       <div id="minorContainer">
+       <p id="songName" class="songFeat">${genre.song}</p>
+       <p id="auth" class="songFeat">${genre.artist}</p>
+       <p id="genre" clas="songFeat">${genre.genre}</p>
+       <audio controls> <source src=${genre.music} type='audio/mpeg'></audio>
+       </div>
+       </div>
+       `
+    }) 
+    
+    const renderRock = `
+    <p>${filterRock}</p>
+    `
+    result.innerHTML = renderRock;
 });
+
+
+var result = document.querySelector('#mscCont');
 
 document.querySelector('#test2').addEventListener('click',(event) => {
-    const oof = document.createElement('p');
-    document.querySelector('#mscCont').innerHTML = ""
-    const elect2 = data.filter(music => {
-        return music.genre == 'Hip-Hop' ;
-    }).forEach((e) => {
-        return oof.innerHTML+=`${e.song} by ${e.artist} <br>`
+    var filterRock = data.filter(function (song){
+        return song.genre === "Hip-Hop";
     })
-
-
-    document.querySelector('#mscCont').appendChild(oof)
+    .map(function (genre) {
+       return  `
+       <div id="songCont">
+       <img class="songFeat image" src=${genre.image}>
+       <div id="minorContainer">
+       <p id="songName" class="songFeat">${genre.song}</p>
+       <p id="auth" class="songFeat">${genre.artist}</p>
+       <p id="genre" clas="songFeat">${genre.genre}</p>
+       <audio controls> <source src=${genre.music} type='audio/mpeg'></audio>
+       </div>
+       </div>
+       `
+    }) 
+    
+    const renderRock = `
+    <p>${filterRock}</p>
+    `
+    result.innerHTML = renderRock;
 });
+
+document.querySelector('#test3').addEventListener('click',(event) => {
+    var filterRock = data.filter(function (song){
+        return song.genre === "rap";
+    })
+    .map(function (genre) {
+       return  `
+       <div id="songCont">
+       <img class="songFeat image" src=${genre.image}>
+       <div id="minorContainer">
+       <p id="songName" class="songFeat">${genre.song}</p>
+       <p id="auth" class="songFeat">${genre.artist}</p>
+       <p id="genre" clas="songFeat">${genre.genre}</p>
+       <audio controls> <source src=${genre.music} type='audio/mpeg'></audio>
+       </div>
+       </div>
+       `
+    }) 
+    
+    const renderRock = `
+    <p>${filterRock}</p>
+    `
+    result.innerHTML = renderRock;
+});
+
+document.querySelector('#test2').click();
+
--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const data = [
       artist: 'Howard The Alien',
       genre: 'Electronic',
       song: 'Money Longer Alien',
-      image: 'https://i.ytimg.com/vi/EBxNE2G640Q/maxresdefault.jpg'
+      image: 'https://www.dailydot.com/wp-content/uploads/2018/07/howard-the-alien.jpg'
     },
     {
         artist: 'Smash Mouth',
@@ -32,42 +32,79 @@ const data = [
         artist: 'SayMaxWell, MiatriSs5',
         genre: 'Electronic',
         song: 'Before the bhombs fell',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+        image: 'https://www.infowars.com/wp-content/uploads/2016/12/trump-time225.jpg'
       },
+
       {
-        artist: 'Tim The Donut',
+        artist: 'Shrek',
         genre: 'Hip-Hop',
-        song: 'Money Happu Man',
-        image: 'https://i.ytimg.com/vi/EBxNE2G640Q/maxresdefault.jpg'
+        song: 'My Swamp',
+        image: 'https://www.dreamworksanimation.com/wp-content/uploads/2014/10/shrek-watch-shrek4.jpg'
       },
       {
           artist: 'Smash Pies',
           genre: 'Hip-Hop',
           song: 'Happu-Starzs',
-          image: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/16/All_star.jpg/220px-All_star.jpg'
+          image: 'http://mcpedl.com/wp-content/uploads/2017/05/fidget-spinner-2.jpg 700w, http://mcpedl.com/wp-content/uploads/2017/05/fidget-spinner-2-300x142.jpg 300w, http://mcpedl.com/wp-content/uploads/2017/05/fidget-spinner-2-520x245.jpg 520w, http://mcpedl.com/wp-content/uploads/2017/05/fidget-spinner-2-720x340.jpg 720w'
         },
         {
-          artist: 'TWeen3',
+          artist: 'cas',
           genre: 'Hip-Hop',
-          song: 'Ocean Women',
-          image: 'http://images.genius.com/2e3c9836a7db1db0063978358e19659d.640x640x1.jpg'
+          song: 'the oof noob',
+          image: 'https://ih1.redbubble.net/image.475371458.6080/flat,1000x1000,075,f.jpg'
         },    {
-          artist: 'Gary Jules4',
+          artist: 'the red cas',
           genre: 'Hip-Hop',
           song: 'Happu Oof',
-          image: 'https://images.rapgenius.com/9c5b21ae6fa3da94704da3777f87e0be.400x400x1.jpg'
+          image: 'https://t5.rbxcdn.com/31d578de728cbc4b9a38033a7a346f14'
         },    {
           artist: 'SayMaxWell, MiatriSs5',
           genre: 'Hip-Hop',
           song: 'We Are Number Loof',
-          image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
+          image: 'https://c-7npsfqifvt34x24jnhjyx2esbolfsx2edpn.g00.ranker.com/g00/3_c-7x78x78x78.sbolfs.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fjnhjy.sbolfs.dpnx2fvtfs_opef_jnhx2f61176x2f2112391607x2fpsjhjobmx2fnjoetfu-pwfs-nbuufs-qipup-v2x3fx78x3d761x26rx3d71x26gnx3dkqhx26gjux3ddspqx26dspqx3dgbdftx26j21d.nbslx3djnbhf_$/$/$/$/$/$'
         },
         {
         artist: 'SayMaxWell, MiatriSs5',
         genre: 'Hip-Hop',
         song: 'Life as we know it',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/51F%2BIy3Q5XL._SS500.jpg'
-      }
+        image:'https://c-7npsfqifvt34x24jnhjyx2esbolfsx2edpn.g00.ranker.com/g00/3_c-7x78x78x78.sbolfs.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fjnhjy.sbolfs.dpnx2fvtfs_opef_jnhx2f61176x2f2112391729x2fpsjhjobmx2fhfpnfusz-pwfs-hznobtjvnt-qipup-v2x3fx78x3d761x26rx3d71x26gnx3dkqhx26gjux3ddspqx26dspqx3dgbdftx26j21d.nbslx3djnbhf_$/$/$/$/$/$'
+      },
 
+      {
+
+        artist: 'roblox oof',
+        genre:'rap',
+        song:'the roblox life',
+        image:'https://t3.rbxcdn.com/6a5ad7e7f0f9b5686444713141021464'
+      },
+      {
+        artist: 'unknown dude',
+        genre: 'rap',
+        song: 'hotline bling',
+        image: 'https://c-7npsfqifvt34x24jnhjyx2esbolfsx2edpn.g00.ranker.com/g00/3_c-7x78x78x78.sbolfs.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fjnhjy.sbolfs.dpnx2fvtfs_opef_jnhx2f61176x2f2112391731x2fpsjhjobmx2fx78fmm-wfhfubcmft-bsf-ifbmuiz-qipup-v2x3fx78x3d761x26rx3d61x26gnx3dkqhx26gjux3ddspqx26dspqx3dgbdftx26j21d.nbslx3djnbhf_$/$/$/$/$/$'
+      },
+      {
+          artist: 'spongebob',
+          genre: 'rap',
+          song: 'who lives under a pineable under the sea',
+          image: 'https://c-7npsfqifvt34x24jnhjyx2esbolfsx2edpn.g00.ranker.com/g00/3_c-7x78x78x78.sbolfs.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fjnhjy.sbolfs.dpnx2fvtfs_opef_jnhx2f61176x2f2112391744x2fpsjhjobmx2fqboebnpojvn-qipup-v2x3fx78x3d761x26rx3d61x26gnx3dkqhx26gjux3ddspqx26dspqx3dgbdftx26j21d.nbslx3djnbhf_$/$/$/$/$/$'
+        },
+        {
+          artist: 'squidward',
+          genre: 'rap',
+          song: 'squidward dance',
+          image: 'https://c-7npsfqifvt34x24jnhjyx2esbolfsx2edpn.g00.ranker.com/g00/3_c-7x78x78x78.sbolfs.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fjnhjy.sbolfs.dpnx2fvtfs_opef_jnhx2f61176x2f2112391742x2fpsjhjobmx2fes-op-dijmm-qipup-v2x3fx78x3d761x26rx3d61x26gnx3dkqhx26gjux3ddspqx26dspqx3dgbdftx26j21d.nbslx3djnbhf_$/$/$/$/$/$'
+        },    {
+          artist: 'mr krabs',
+          genre: 'rap',
+          song: 'money money money',
+          image: '"https://bit.ly/2OBeVyU"'
+        },    {
+          artist: 'soybrean',
+          genre: 'rap',
+          song: 'somebody once told me',
+          image: 'https://bit.ly/2QM5Vbr'
+        },
+        
       
   ]
